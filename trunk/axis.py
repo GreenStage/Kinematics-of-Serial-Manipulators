a36bed13de04492aa09ebac5c455a5e14dd66396
Eduardo Gomes <eduardo.l.gomes@tecnico.ulisboa.pt>
2018-04-04 14:13:19 +0100

    atualizado

--- trunk/axis.py
+++ trunk/axis.py
@@ -1,7 +1,7 @@
-#
 from math import cos,sin,atan2,sqrt
 class Axis:
     #Euler ZYZ
+    @staticmethod    
     def angles_zyz(p):
         beta = atan2(sqrt( p[0][2]**2 + p[1][2]**2 ),p[2][2])
         sinBeta = sin(beta)
@@ -25,8 +25,9 @@ class Axis:
             'gamma': gamma
         }
 
+    @staticmethod
     def invert_zyz(alpha,beta,gamma):
-        retval = [[0,0,0],[0,0,0],[0,0,0]
+        retval = [[0,0,0],[0,0,0],[0,0,0]]
         retval[0][0] = cos(gamma) * cos(alpha) * cos(alpha) - sin(alpha) * sin(gamma)
         retval[0][1] = - sin(gamma) * cos(alpha) * cos(alpha) - sin(alpha) * cos(gamma)
         retval[0][2] = cos(gamma) * sin(beta)
@@ -39,6 +40,7 @@ class Axis:
         return retval
 
     #Fixed XYZ
+    @staticmethod
     def angles_xyz(p):
         beta = atan2(-p[2][0],sqrt( p[0][0]**2 + p[1][0]**2 ))
         sinBeta = sin(beta)
--- trunk/inverse.py
+++ trunk/inverse.py
@@ -1,9 +1,15 @@
 from axis import Axis
+import json
+from frame import Frame
 from matrices import Matrices
+from math import atan2,cos,sin,acos,sqrt
+
+pi = 3.14159265358979323846264338
 
 def inverse_kinematics(x,y,z,alpha,beta,gamma,**frame_file):
-    angles = []
-    displ = [x,y,z]
+    angles = {}
+    frames = []
+    pos = [x,y,z]
     filename =  frame_file.pop('input', 'serial_manipulator_6.json')
 
     with open(filename) as input:
@@ -21,11 +27,45 @@ def inverse_kinematics(x,y,z,alpha,beta,gamma,**frame_file):
         for prototype in frames_prototype:
             frames.append(Frame(prototype['displacement'],prototype['previous_relation']))
   
-    T = Axis.invert_zyz(alpha,beta,gamma):
+    T = Axis.invert_zyz(alpha,beta,gamma)
 
-    for i in range(len(frames) - 1,-1,-1):
-        #Remove displacement
-        disp = Matrices.multiply(frames[i].displacement,frames[i].parent_relation_m)
-        
-        
+#compute teta1
+    angles['teta1'] = []
+    angles['teta1'].append( {
+        'value' : atan2( pos[1],pos[0] ),
+        'teta2' : [],
+        'teta3' : []
+    } )
+    
+    angles['teta1'].append( {
+        'value' : atan2( -pos[1],-pos[0] ),
+        'teta2' : [],
+        'teta3' : []
+    } )
 
+#compute teta2,teta3
+#rotate plane around z to zy for each teta1
+    for a in angles['teta1']:
+        ang = pi/2 - a['value']
+        p = [pos]
+        np = Matrices.multiply(p,[[cos(ang),-sin(ang),0],
+                                      [sin(ang), cos(ang), 0],
+                                      [0,        0,        1]])
+        new_p = np[0]
+        #subtract the displacement created by frame 4 - 155
+        d1 = sum(frames[2].displacement)
+        d2 = sum(frames[4].displacement) - sum(frames[3].displacement)
+        d0 = sqrt(new_p[1]**2 + new_p[2]**2)
+
+        a0 = atan2(new_p[1],new_p[2])
+        a1 = acos( (d1**2 + d0**2 - d2**2) / (2*d1*d0) )
+        a2 = acos( (d1**2 + d2**2 - d0**2) / (2*d1*d2) )
+
+        a['teta2'].append({'value': a0 + a2})
+        a['teta2'].append({'value': a0 - a2})
+        a['teta3'].append({'value': a1})
+        a['teta3'].append({'value': 2*pi - a1})    
+
+    print(angles)
+
+inverse_kinematics(170,0,150,1.3,1.2,1.4)
